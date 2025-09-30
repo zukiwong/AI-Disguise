8a640731cce10eb8f7979510e1768e6c00d6cd1d
zuki <wxq839190594@gmail.com>
2025-09-30 14:42:16 +1300

    Add Vercel Analytics

--- wordshelf/src/main.jsx
+++ wordshelf/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
